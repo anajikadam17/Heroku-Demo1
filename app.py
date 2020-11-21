88d30c35082192ee50a013a6409a9ff9bc9ee1fd
anajikadam17 <anajikadam17@gmail.com>
2020-11-21 22:10:35 +0530

    commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def home():
+    return "YAY!!! its working"
+
+
+@app.route('/add',methods=['GET'])
+def addNum():
+    a = 2
+    b = 3
+    return "The sm of {} and {} is {}".format(a,b,a+b)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
